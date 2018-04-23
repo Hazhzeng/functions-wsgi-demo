059c14e468ef49030107de50d74ebb26d8d93c6c
Roger Tsang <zenghanzhang46@gmail.com>
2018-04-23 21:31:30 +1000

    Added: flask-login framework

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
-
+import logging
 
 TEMPLATE_DIR = os.path.join('./templates')
 STATIC_DIR = os.path.join('./static')
+
 
 app = Flask(
     __name__,
@@ -15,7 +17,8 @@ app = Flask(
 app.config.from_object(__name__)
 
 app.config.update(dict(
-    SQLALCHEMY_DATABASE_URI='sqlite:////{}'.format(os.path.join(app.root_path, 'sql', 'chat.db')),
+    SQLALCHEMY_DATABASE_URI='sqlite:////{}'\
+        .format(os.path.join(app.root_path, 'sql', 'chat.db')),
     SQLALCHEMY_TRACK_MODIFICATIONS='False',
     DATABASE_INIT_SQL=os.path.join(app.root_path, 'sql', 'init.sql'),
     SECRET_KEY='development',
@@ -24,6 +27,12 @@ app.config.update(dict(
 ))
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+logging.basicConfig(format='%(asctime)-15s %(message)s')
+logger = logging.getLogger('Roject Logger')
 
 import project.views.views
 import project.views.api
