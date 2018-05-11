865b6b6e6a9a199a04571dbe72cfbabb14999ac5
Roger Tsang <zenghanzhang46@gmail.com>
2018-05-12 01:59:48 +1000

    Fix: info page

--- project/static/js/containers/InfoContainer.js
+++ project/static/js/containers/InfoContainer.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 
 import Avatar from 'material-ui/Avatar';
 import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui/styles';
 
 import Blog from '../components/Contents/Blog';
 
-const styles = theme => ({
-  paper: {
-    padding: theme.spacing.unit * 5,
-    textAlign: 'left',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-  },
+const styles = () => ({
   avatar: {
     width: 120,
     height: 120,
@@ -36,35 +29,34 @@ class InfoContainer extends Component {
   }
 
   _renderInfomation() {
-    const { classes } = this.props;
     const title = 'Who am I? What am I doing? Where will I go?';
     const text = `
       ### Who am I?
-      In someday around 1990s, my mom decide to dump me into this cruel world,
-      partially intentionally, that was the day I born.
+      In someday around 1990s, my mom decided to dump me into this cruel world,
+      intentionally. That was the day when I was born.
 
-      Spending most of my time struggling before my 18, got accepted by a
-      university called University of New South Wale a university known to the
-      world but not in China. I mean, it is not taken from granted for a uni to
-      get to top 50 in QS ranking, but, come on, our uni has changed its name
-      for 3 times in recent 4 years, ordering by time:
+      Spending most of my time struggling, got accepted by a university called
+      UNSW, a well know university but not to you.
+      I mean, it is not taken from granted for a university to get to top 50 in
+      QS ranking by dumping a lot of money into it.
+
+      Interestingly, our university has changed its name
+      for 3 times in the most recent 4 years, ordering by time:
 
       1. University of New South Wales
       2. University of New South Wales Australia
       3. University of New South Wales Sydney
 
-      4  years later, I got my Bachelar of Engineering, more specifically,
-      majoring Computer Engineering. Not far until I found that uni has taugh me
-      insufficient but fundamental knowledge, here I start my web dev career.
+      4  years later, I got my Bachelar of Engineering.
+      Not far until I found that uni has taugh me insufficient but
+      fundamental knowledge, here I start my web dev project.
 
       # *WHAT A LOSER...*
     `;
 
     return (
       <Grid item sm={12} lg={12} key={'info.information'}>
-        <Paper className={classes.paper}>
-          <Blog title={title} text={text} />
-        </Paper>
+        <Blog title={title} text={text} />
       </Grid>
     );
   }
