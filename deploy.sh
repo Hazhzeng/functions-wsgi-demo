0da834de13050cc9feb908fd460c2f557963d746
Roger <roger@ip-172-31-7-211.ap-southeast-2.compute.internal>
2018-05-12 06:07:51 +0000

    Added: version control on deployment

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,17 @@
 PROJECT=Roject
 PROJECT_ENV=RojectEnv
 
+VERSION=$(git log --oneline | head -n 1 | cut -d' ' -f1)
+
 source ~/$PROJECT_ENV/bin/activate
 
 cd ~/$PROJECT
+echo "$VERSION" > version.txt
 pip install -r requirements.txt
 
 npm install
 npm run build
+
+sudo systemctl restart roject
 
 deactivate
