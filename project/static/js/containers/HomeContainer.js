4e4923350309a252cb5be6459c4f9c8967dfcb93
Roger Tsang <zenghanzhang46@gmail.com>
2018-05-14 23:45:49 +1000

    Added: use DP to balance blog panel

--- project/static/js/containers/HomeContainer.js
+++ project/static/js/containers/HomeContainer.js
@@ -23,6 +23,10 @@ const styles = theme => ({
     marginRight: theme.spacing.unit,
     width: 200,
   },
+  hr: {
+    clear: 'both',
+    visibility: 'hidden',
+  }
 });
 
 class HomeContainer extends Component {
@@ -32,13 +36,13 @@ class HomeContainer extends Component {
     const t = new Date();
     this.state = {
       isDateValid: true,
-      date: `${dateToString(t)}`
+      date: `${dateToString(t)}`,
     };
 
     this.onDateChange = this.onDateChange.bind(this);
     this.pullSubmit = _.debounce(this.pullSubmit, 1000);
 
-    /* API call */
+    /* API call to fetch blog */
     props.pull(this.state.date);
   }
 
@@ -76,23 +80,73 @@ class HomeContainer extends Component {
     );
   }
 
-  _renderBlogs() {
-    const { classes, blogs } = this.props;
-    const renderedBlogs = blogs.map(blog => (
-      <Grid item sm={12} lg={6} key={blog.last_update}>
+  _renderBlog(blog) {
+    const { classes } = this.props;
+    return (
+      <div key={blog.last_update}>
         <Paper className={classes.paper}>
           <Blog title={blog.title} time={blog.last_update} text={blog.text} />
         </Paper>
-      </Grid>
-    ));
-    return renderedBlogs;
+        <hr className={classes.hr} />
+      </div>
+    );
+  }
+
+  dpRenderBlogs(remains, pickedLeft, pickedRight, heightLeft, heightRight) {
+    if (remains.length == 0) {
+      return ({
+        left: heightLeft,
+        right: heightRight,
+        leftBlogs: pickedLeft,
+        rightBlogs: pickedRight
+      });
+    }
+
+    const blog = remains[0];
+    const blogHeight = blog.text.split(/\r\n|\r|\n/).length;
+
+    const pickLeft = this.dpRenderBlogs(
+      remains.slice(1),
+      [...pickedLeft, blog],
+      [...pickedRight],
+      heightLeft + blogHeight,
+      heightRight
+    );
+
+    const pickRight = this.dpRenderBlogs(
+      remains.slice(1),
+      [...pickedLeft],
+      [...pickedRight, blog],
+      heightLeft,
+      heightRight + blogHeight
+    );
+
+    const pickLeftDiff = Math.abs(pickLeft.left - pickLeft.right);
+    const pickRightDiff = Math.abs(pickRight.left - pickRight.right);
+
+    if (pickLeftDiff < pickRightDiff) {
+      return pickLeft;
+    } else {
+      return pickRight;
+    }
   }
 
   render() {
+    const { blogs } = this.props;
+    let result = { leftBlogs: [], rightBlogs: []};
+    if (blogs && blogs.length > 0) {
+      result = this.dpRenderBlogs(blogs, [], [], 0, 0);
+    }
+    
     return (
-      <Grid container spacing={24}>
+      <Grid container spacing={24} direction="row">
         {this._renderMemoryPicker()}
-        {this._renderBlogs()}
+        <Grid item sm={12} lg={6} key="left-blog-section">
+          {result.leftBlogs.map(blog => this._renderBlog(blog))}
+        </Grid>
+        <Grid item sm={12} lg={6} key="right-blog-section">
+          {result.rightBlogs.map(blog => this._renderBlog(blog))}
+        </Grid>
       </Grid>
     );
   }
