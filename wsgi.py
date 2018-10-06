f02997aaacf451dd1676eac6b3d915d67a84a7af
Roger Tsang <zenghanzhang46@gmail.com>
2018-10-06 14:50:20 +1000

    Setup: gunicorn logger

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
+import logging
 from project import app
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
